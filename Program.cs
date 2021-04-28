2ed1c70c8a672035e7f83eb2b7f4d6ac9e0b48bb
jonasnapoles <jno@outlook.pt>
2021-04-28 07:46:11 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           byte comprimento=0;
+           byte largura=0;
+           double area=0;
+
+            Console.WriteLine("Programa para Calcular área!");
+            Console.WriteLine("Insira o comprimento:");
+            comprimento = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Insira a largura:");
+            largura = byte.Parse(Console.ReadLine());
+            area = comprimento * largura;
+            
+            Console.WriteLine($"A área total é {area}");
+            Console.WriteLine("Fim do programa");
+            Console.ReadKey();
+               
+            
+            
+
+        }
+    }
+}
